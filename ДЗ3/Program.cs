ecb53a1324c01256833f526f9df9621d797e8149
Катерина <katttum@ya.ru>
2023-07-05 23:29:26 +0300

    таблица кубов от 1 до N

---
+++ ДЗ3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Таблица кубов чисел от 1 до {0}:", N);
+
+        for (int i = 1; i <= N; i++)
+        {
+            int cube = i * i * i;
+            Console.Write(cube + " ");
+        }
+
