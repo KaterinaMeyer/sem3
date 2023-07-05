3e1f3fe491a2799265527c6eb0b84044a6b14bca
Катерина <katttum@ya.ru>
2023-07-05 23:06:17 +0300

    полиндром-2

--- ДЗ1/Program.cs
+++ ДЗ1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿       Console.Write("Введите пятизначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int a = number / 10000;
+        int b = (number / 1000) % 10;
+        int c = (number / 10) % 10;
+        int d = number % 10;
+
+        if (a == d && b == c)
+        {
+            Console.WriteLine("Да, число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Нет, число не является палиндромом.");
+        }
